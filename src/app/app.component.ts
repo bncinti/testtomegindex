ad130492cf99ebaa83f791a2c56200ea60cf81a0
Ban Cintia <bncincia@gmail.com>
2022-01-13 10:35:42 +0100

    kesz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
+/*
+* File: app.component.ts
+* Author: Bán Cintia
+* Copyright: 2022, Bán Cintia
+* Group: Szoft II/N
+* Date: 2022.01-13
+* Github: https://github.com/bncinti/
+* Licenc: GNU GPL
+*/
+
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testtomegindex';
+
+  bmiVisible = false;
+
+  bmiForm !: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.bmiForm = this.formBuilder.group({
+      weight: [''],
+      height: [''],
+	    bmi :['']
+      
+    });
+  }
+
+  
+
+  onClickSzamit(){
+    let weight=Number(this.bmiForm.value.weight) 
+    let height =Number(this.bmiForm.value.height) 
+    let bmi= weight/Math.pow(height/100,2);
+  console.log(bmi)
+  this.bmiVisible = true;
+  }
 }
+
